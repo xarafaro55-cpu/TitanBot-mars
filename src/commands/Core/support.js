97a55f2dc5f97c1859acbda5f8eff45702d529ae
xarafaro55-cpu <xarafaro55@gmail.com>
2026-07-10 20:44:08 +0300

    Update support.js

--- src/commands/Core/support.js
+++ src/commands/Core/support.js
@@ -3,7 +3,7 @@ import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
-const SUPPORT_SERVER_URL = "[Mars](https://discord.gg/C4nFwhGEJn)";
+const SUPPORT_SERVER_URL = "(https://discord.gg/C4nFwhGEJn)";
 export default {
     data: new SlashCommandBuilder()
     .setName("support")
